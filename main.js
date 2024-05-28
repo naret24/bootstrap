df8ff760cef9e8d78088e88522f5c856ffece558
naret24 <narelygarcialuis@gmail.com>
2024-05-28 12:00:48 -0600

    pagina

---
+++ main.js
@@ -0,0 +1,17 @@
+//ESCRIBIR MIS FUNCIONES
+//FUNCION 1.GENERAR UN COLOR ALEATORIO
+function generaColor(){
+//generar mi valor de RGB por separado y guardarlos en variables
+const rojo ="" ;
+const verde ="" ;
+const azul = "";
+}
+//--->RBG(X,Y,Z)
+
+//FUNCION 2. CAMBIAR EL COLOR DE FONDO DEL CUERPO DE LA PAGINA
+function cambioBackground(){
+    //
+}
+
+//BUSCAR EL BOTON EN MI html POR SU CLASE
+// CADA VEZ QUE MMI BOTON SEA ACCIONADO, VOY A LLAMAR A EJECUTAR MI FUNCION cambioBacground;
